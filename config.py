2684755ad37e80a8ec27dc4bd50b6658fd3bce1d
krysmathis <krysmathis@gmail.com>
2021-04-24 10:11:24 -0500

    updated requirements and added manage.py for db

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI="sqlite:///database.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import time
 from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    __tablename__ = 'commitment_users'
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    usertype = db.Column(db.Integer())
+    latest_action_date = db.Column(db.DateTime())
+
+    def __init__(self,username):
+        self.username = username
+    
+    def __repr__(self):
+        return "<User '{}'".format(self.username)
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
